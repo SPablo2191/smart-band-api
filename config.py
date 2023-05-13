743a50b060cfeb0d4809e09322bf2ac41f0fb331
Pablo Sandoval <pablosandoval2191@gmail.com>
2023-05-13 15:15:49 -0500

    feat: db configuration

---
+++ config.py
@@ -0,0 +1,5 @@
+from dotenv import load_dotenv
+import os
+def config(app):
+    load_dotenv()
+    app.config['SQLALCHEMY_DATABASE_URI'] = f"postgresql://{os.getenv('DB_USER')}:{os.getenv('DB_PASSWORD')}@{os.getenv('DB_HOST')}/{os.getenv('DB_NAME')}"
---
+++ database/db.py
@@ -0,0 +1,27 @@
+from flask_sqlalchemy import SQLAlchemy
+from flask_marshmallow import Marshmallow
+from sqlalchemy.pool import QueuePool
+ma = Marshmallow()
+db = SQLAlchemy()
+
+
+def initialize_db(app):
+    db.init_app(app)
+
+
+def add_engine(app):
+    with app.app_context():
+        app.config["SQLALCHEMY_POOL_SIZE"] = 5
+        app.config["SQLALCHEMY_MAX_OVERFLOW"] = 5
+        engine = db.create_engine(
+            app.config["SQLALCHEMY_DATABASE_URI"],
+            poolclass=QueuePool,
+            pool_size=app.config["SQLALCHEMY_POOL_SIZE"],
+            max_overflow=app.config["SQLALCHEMY_MAX_OVERFLOW"],
+            pool_timeout=60
+        )
+        db.session.configure(bind=engine)
+
+
+def init_app(app):
+    ma.init_app(app)
