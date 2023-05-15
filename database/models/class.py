f04231c0796c9a00bbc528d22a7d1ddb43fe2fff
Pablo Sandoval <pablosandoval2191@gmail.com>
2023-05-15 16:21:02 -0500

    feat: Class class added

--- database/models/class.py
+++ database/models/class.py
@@ -3,6 +3,6 @@ from db import db
 class Class(db.model):
     __tablename__ = "class"
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(50), nullable=False)
+    name = db.Column(db.String(100), nullable=False)
     status = db.Column(db.Boolean, default=True, nullable=False)
     registerDate = db.Column(db.DateTime, default=datetime.utcnow())
