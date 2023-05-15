c8226b558757406c41ec586fad22f67281ef139f
Pablo Sandoval <pablosandoval2191@gmail.com>
2023-05-15 17:13:40 -0500

    feat: student and diseases

---
+++ database/models/disease_student.py
@@ -0,0 +1,11 @@
+import datetime
+from db import db
+
+
+class DiseaseStudent(db.model):
+    __tablename__ = "disease_student"
+    id = db.Column(db.Integer, primary_key=True)
+    student_id = db.Column(db.Integer, db.ForeignKey("student.id"), nullable=False)
+    disease_id = db.Column(db.Integer, db.ForeignKey("disease.id"), nullable=False)
+    status = db.Column(db.Boolean, default=True, nullable=False)
+    register_date = db.Column(db.DateTime, default=datetime.utcnow())
--- database/models/student.py
+++ database/models/student.py
@@ -1,13 +1,18 @@
 import datetime
 from db import db
+
+
 class Student(db.model):
     __tablename__ = "student"
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100), nullable=False)
     last_name = db.Column(db.String(100), nullable=False)
-    age = db.Column(db.Integer,nullable = False)
-    weight = db.Column(db.Float,nullable = False)
-    size = db.Column(db.Float,nullable = False)
-    DNI =  db.Column(db.String(15), nullable=False)
+    age = db.Column(db.Integer, nullable=False)
+    weight = db.Column(db.Float, nullable=False)
+    size = db.Column(db.Float, nullable=False)
+    DNI = db.Column(db.String(15), nullable=False)
     status = db.Column(db.Boolean, default=True, nullable=False)
     register_date = db.Column(db.DateTime, default=datetime.utcnow())
+    diseases = db.relationship(
+        "Disease", backref="student", secondary="disease_student", lazy=True
+    )
