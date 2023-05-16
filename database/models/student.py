c531845b8a98014ee0288a4a7836ef7afa2e67a4
Pablo Sandoval <pablosandoval2191@gmail.com>
2023-05-16 16:28:43 -0500

    feat: student and disease schemas

--- database/models/student.py
+++ database/models/student.py
@@ -1,5 +1,7 @@
 from datetime import datetime
 from database.db import db
+from marshmallow import Schema, fields
+from .disease import DiseaseSchema
 
 
 class Student(db.Model):
@@ -17,3 +19,20 @@ class Student(db.Model):
         "Disease", backref="student", secondary="disease_student", lazy=True
     )
     results = db.relationship("Result", backref="student", lazy=True)
+
+
+class StudentSchema(Schema):
+    id = fields.Integer()
+    name = fields.String()
+    last_name = fields.String()
+    age = fields.Float()
+    weight = fields.Float()
+    size = fields.Float()
+    DNI = fields.String()
+    status = fields.Boolean()
+    register_date = fields.Date()
+    diseases = fields.Nested(DiseaseSchema, many=True)
+
+
+student_schema = StudentSchema()
+students_schema = StudentSchema(many=True)
