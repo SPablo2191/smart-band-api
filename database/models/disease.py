ff44ca54546489278cb5511231a4b7fe18b000bd
Pablo Sandoval <pablosandoval2191@gmail.com>
2023-05-16 16:43:27 -0500

    feat: disease school and status test schemas

--- database/models/disease.py
+++ database/models/disease.py
@@ -1,12 +1,15 @@
 from datetime import datetime
 from database.db import db
 from marshmallow import Schema, fields
+
+
 class Disease(db.Model):
     __tablename__ = "disease"
     id = db.Column(db.Integer, primary_key=True)
     description = db.Column(db.String(100), nullable=False)
     status = db.Column(db.Boolean, default=True, nullable=False)
     register_date = db.Column(db.DateTime, default=datetime.utcnow())
+
 
 class DiseaseSchema(Schema):
     id = fields.Integer()
--- database/models/school.py
+++ database/models/school.py
@@ -1,8 +1,18 @@
 from datetime import datetime
 from database.db import db
+from marshmallow import Schema, fields
+
+
 class School(db.Model):
     __tablename__ = "school"
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(50), nullable=False)
     status = db.Column(db.Boolean, default=True, nullable=False)
     register_date = db.Column(db.DateTime, default=datetime.utcnow())
+
+
+class DiseaseSchema(Schema):
+    id = fields.Integer()
+    description = fields.String()
+    status = fields.Boolean()
+    register_date = fields.Date()
