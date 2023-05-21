f5d1064a21b780292c1455629a41e478f26e5294
Pablo Sandoval <pablosandoval2191@gmail.com>
2023-05-21 10:23:07 -0500

    fix: School schema change description to name fields

--- resources/routes.py
+++ resources/routes.py
@@ -1,5 +1,5 @@
 from .schools import SchoolsAPI,SchoolAPI
-url='/api'
+url='/api/'
 def initialize_routes(api):
-     api.add_resource(SchoolsAPI, url+'/schools')
-     api.add_resource(SchoolAPI, url+'/schools/<int:id>')
+     api.add_resource(SchoolsAPI, url+'schools')
+     api.add_resource(SchoolAPI, url+'schools/<int:id>')
