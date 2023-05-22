025d26221cd03791ac9ea3d6fff84cde045c9fda
Pablo Sandoval <pablosandoval2191@gmail.com>
2023-05-21 22:37:59 -0500

    feat: add teachers route

--- resources/routes.py
+++ resources/routes.py
@@ -1,5 +1,7 @@
 from .schools import SchoolsAPI,SchoolAPI
+from .teachers import TeachersAPI
 url='/api/'
 def initialize_routes(api):
      api.add_resource(SchoolsAPI, url+'schools')
      api.add_resource(SchoolAPI, url+'schools/<int:id>')
+     api.add_resource(TeachersAPI, url+'teachers')
