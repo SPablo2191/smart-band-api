19e120f6fd9e28ef3a435215f01006e634ec3dd4
Pablo Sandoval <pablosandoval2191@gmail.com>
2023-05-22 12:12:27 -0500

    fix: promotions schema added

--- database/models/promotion.py
+++ database/models/promotion.py
@@ -1,18 +1,16 @@
 from datetime import datetime
 from database.db import db
 from marshmallow import Schema, fields
-from .student import StudentSchema
+
 class Promotion(db.Model):
     __tablename__ = "promotion"
     id = db.Column(db.Integer, primary_key=True)
     promotion_year = db.Column(db.DateTime, nullable=False)
-    students = db.relationship(
-        "Student", backref="promotion", secondary="promotion_student", lazy=True
-    )
     status = db.Column(db.Boolean, default=True, nullable=False)
     class_id = db.Column(db.Integer, db.ForeignKey("class.id"), nullable=False)
     prom_class = db.relationship("Class", backref="promotion", uselist=False)
     register_date = db.Column(db.DateTime, default=datetime.utcnow())
+
 
 
 class PromotionSchema(Schema):
@@ -20,7 +18,7 @@ class PromotionSchema(Schema):
     promotion_year = fields.Date()
     status = fields.Boolean()
     register_date = fields.Date()
-    students = fields.Nested(StudentSchema)
+    # students = fields.Nested(StudentSchema)
 
 
 promotion_schema = PromotionSchema()
--- database/models/student.py
+++ database/models/student.py
@@ -4,6 +4,7 @@ from marshmallow import Schema, fields
 from .disease import DiseaseSchema
 from .disease_student import DiseaseStudent
 from .result import ResultSchema
+from .promotion import PromotionSchema
 class Student(db.Model):
     __tablename__ = "student"
     id = db.Column(db.Integer, primary_key=True)
@@ -17,10 +18,8 @@ class Student(db.Model):
     seat_height = db.Column(db.Float,nullable=False)
     waist = db.Column(db.Float, nullable=False)
     register_date = db.Column(db.DateTime, default=datetime.utcnow())
-    # diseases = db.relationship(
-    #     "DiseaseStudent", back_populates="student", lazy=True
-    # )
     diseases = db.relationship("DiseaseStudent", back_populates="student", uselist=False)
+    promotions = db.relationship("PromotionStudent", back_populates="student", uselist=False)
     results = db.relationship("Result", backref="student", lazy=True)
 
 
@@ -38,6 +37,7 @@ class StudentSchema(Schema):
     waist = db.Float()
     diseases = fields.Nested(DiseaseSchema,many=True)
     results = fields.Nested(ResultSchema,many=True)
+    promotions = fields.Nested(PromotionSchema,many=True)
 
 student_schema = StudentSchema()
 students_schema = StudentSchema(many=True)
