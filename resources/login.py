dfc43539ec046c85d205a49a7f8b057b4013b8a5
Pablo Sandoval <pablosandoval2191@gmail.com>
2023-05-22 13:36:19 -0500

    feat: login in process

--- resources/login.py
+++ resources/login.py
@@ -1,4 +1,4 @@
-from flask import Response, request
+from flask import Response, request,make_response
 from flask_restful import Resource
 from database.models.teacher import Teacher, teacher_schema, teachers_schema
 from database.db import db
@@ -6,4 +6,13 @@ from functions.encrypt import bcrypt
 
 class LoginAPI(Resource):
     def post(self):
-        pass
+        body = request.get_json()
+        email = body['email']
+        password = body['password']
+        try:
+            user = Teacher.query.filter(Teacher.email==email).first()
+            print(user.password,password)
+            if(bcrypt.check_password_hash(user.password, password)):
+                print("Inicio de sesion exitoso")
+        except Exception as e:
+            return make_response({"error": str(e)}, 404)
