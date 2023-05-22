7a86ef84ff3806cfb476b3a19abe3d24dc361507
Pablo Sandoval <pablosandoval2191@gmail.com>
2023-05-22 13:55:23 -0500

    feat: login successfully

--- resources/login.py
+++ resources/login.py
@@ -1,18 +1,24 @@
-from flask import Response, request,make_response
+from flask import Response, request, make_response
 from flask_restful import Resource
 from database.models.teacher import Teacher, teacher_schema, teachers_schema
 from database.db import db
 from functions.encrypt import bcrypt
+from flask_jwt_extended import create_access_token, jwt_required
+
 
 class LoginAPI(Resource):
     def post(self):
         body = request.get_json()
-        email = body['email']
-        password = body['password']
+        email = body["email"]
+        password = body["password"]
         try:
-            user = Teacher.query.filter(Teacher.email==email).first()
-            print(user.password,password)
-            if(bcrypt.check_password_hash(user.password, password)):
-                print("Inicio de sesion exitoso")
+            user = Teacher.query.filter(Teacher.email == email).first()
+            if not bcrypt.check_password_hash(user.password, password):
+                return make_response({"error": "Contraseña incorrecta"}, 401)
+            access_token = create_access_token(identity=user.DNI)
         except Exception as e:
             return make_response({"error": str(e)}, 404)
+        finally:
+            return Response(
+                access_token, mimetype="application/json", status=200
+            )
