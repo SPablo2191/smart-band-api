757b1e2c1e8bb792575e353a40cd1c5625d53897
Pablo Sandoval <pablosandoval2191@gmail.com>
2023-05-27 12:13:33 -0500

    fix: main route goes to swagger

--- functions/swagger.py
+++ functions/swagger.py
@@ -10,7 +10,7 @@ def api_spec_conf(app):
         plugins=[MarshmallowPlugin()],
         openapi_version='2.0.0'
     ),
-    'APISPEC_SWAGGER_URL': '/swagger/',  # URI to access API Doc JSON
-    'APISPEC_SWAGGER_UI_URL': '/swagger-ui/'  # URI to access UI of API Doc
+    'APISPEC_SWAGGER_URL': '/swagger/', 
+    'APISPEC_SWAGGER_UI_URL': '/'  
     })
     docs.init_app(app)
