6b5f33a975ecfdee93ed83a0f95fbd769cd2691e
Pablo Sandoval <pablosandoval2191@gmail.com>
2023-05-27 12:07:53 -0500

    feat: routes and docs of disease

--- resources/routes.py
+++ resources/routes.py
@@ -3,6 +3,7 @@ from .teachers import TeachersAPI
 from .register import RegisterAPI
 from .login import LoginAPI
 from .students import StudentAPI,StudentsAPI
+from .diseases import DiseaseAPI,DiseasesAPI
 from functions.swagger import docs
 url = "/api/"
 
@@ -15,12 +16,16 @@ def initialize_routes(api):
     api.add_resource(SchoolAPI, url + "schools/<int:id>")
     api.add_resource(StudentsAPI, url + "students")
     api.add_resource(StudentAPI, url + "students/<int:id>")
+    api.add_resource(DiseasesAPI, url + "diseases")
+    api.add_resource(DiseaseAPI, url + "diseases/<int:id>")
     # add docs
     docs.register(SchoolAPI)
     docs.register(SchoolsAPI)
     docs.register(TeachersAPI)
     docs.register(StudentAPI)
     docs.register(StudentsAPI)
+    docs.register(DiseaseAPI)
+    docs.register(DiseasesAPI)
     docs.register(LoginAPI)
     docs.register(RegisterAPI)
 
