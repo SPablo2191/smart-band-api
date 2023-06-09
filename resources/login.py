e442033ea72e284243e40164a9aee085c5426a11
Pablo Sandoval <pablosandoval2191@gmail.com>
2023-06-08 21:21:12 -0500

    fix: pass user_id

--- resources/login.py
+++ resources/login.py
@@ -21,8 +21,7 @@ class LoginAPI(MethodResource,Resource):
             if not bcrypt.check_password_hash(user.password, password):
                 return make_response({"error": "Contraseña incorrecta"}, 401)
             expires = datetime.timedelta(minutes=30)
-
-            access_token = create_access_token(identity=user.DNI,expires_delta=expires)
-            return make_response({'access_token': access_token},200)
+            access_token = create_access_token(identity=user.DNI)
+            return make_response({'access_token': access_token, 'user_id': user.id},200)
         except Exception as e:
             return make_response({"error": str(e)}, 404)
