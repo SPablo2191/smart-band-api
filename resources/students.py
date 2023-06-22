cac72020d1cf544c64011de49e33fcef69ba49ce
Pablo Sandoval <pablosandoval2191@gmail.com>
2023-06-21 19:47:09 -0500

    fix: commented kwargs in student post and put

--- resources/students.py
+++ resources/students.py
@@ -1,21 +1,29 @@
 from flask import Response, request
 from flask_restful import Resource
-from database.models.student import Student,student_schema,students_schema
+from database.models.student import Student, student_schema, students_schema
 from database.db import db
 from flask_apispec.views import MethodResource
 from flask_apispec import marshal_with, doc, use_kwargs
 from flask_jwt_extended import jwt_required
-class StudentsAPI(MethodResource,Resource):
-    @doc(description='Petición GET para recuperar los estudiantes registrados', tags=['Student'])
+
+
+class StudentsAPI(MethodResource, Resource):
+    @doc(
+        description="Petición GET para recuperar los estudiantes registrados",
+        tags=["Student"],
+    )
     @marshal_with(students_schema)
     @jwt_required()
     def get(self):
-        students = Student.query.filter(Student.status == True).order_by(Student.id).all()
+        students = (
+            Student.query.filter(Student.status == True).order_by(Student.id).all()
+        )
         return Response(
             students_schema.dumps(students), mimetype="application/json", status=200
         )
-    @doc(description='Petición POST para añadir un nuevo estudiante', tags=['Student'])
-    @use_kwargs(student_schema, location=('json'))
+
+    @doc(description="Petición POST para añadir un nuevo estudiante", tags=["Student"])
+    # @use_kwargs(student_schema, location=('json'))
     @marshal_with(student_schema)
     @jwt_required()
     def post(self, **kwargs):
@@ -27,8 +35,12 @@ class StudentsAPI(MethodResource,Resource):
             student_schema.dumps(new_student), mimetype="application/json", status=200
         )
 
-class StudentAPI(MethodResource,Resource):
-    @doc(description='Petición GET para recuperar un estudiante por su ID', tags=['Student'])
+
+class StudentAPI(MethodResource, Resource):
+    @doc(
+        description="Petición GET para recuperar un estudiante por su ID",
+        tags=["Student"],
+    )
     @marshal_with(student_schema)
     @jwt_required()
     def get(self, id):
@@ -37,8 +49,11 @@ class StudentAPI(MethodResource,Resource):
             student_schema.dumps(school), mimetype="application/json", status=200
         )
 
-    @doc(description='Petición PUT para actualizar un estudiante por su ID', tags=['Student'])
-    @use_kwargs(student_schema, location=('json'))
+    @doc(
+        description="Petición PUT para actualizar un estudiante por su ID",
+        tags=["Student"],
+    )
+    # @use_kwargs(student_schema, location=('json'))
     @marshal_with(student_schema)
     @jwt_required()
     def put(self, id, **kwargs):
@@ -52,7 +67,11 @@ class StudentAPI(MethodResource,Resource):
             mimetype="application/json",
             status=200,
         )
-    @doc(description='Petición DELETE para eliminar un estudiante por su ID', tags=['Student'])
+
+    @doc(
+        description="Petición DELETE para eliminar un estudiante por su ID",
+        tags=["Student"],
+    )
     @marshal_with(student_schema)
     @jwt_required()
     def delete(self, id):
